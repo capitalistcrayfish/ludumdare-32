209e0f14af703a169232b2e743edae014d159c38
Marnix Massar <weloxux@glowbug.nl>
2015-04-18 03:32:56 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,22 @@
+-----------------------------
+-- The Capitalist Crayfish --
+--   Ludum Dare 32 (2015)  --
+-----------------------------
+-- Programming: Marnix "Weloxux" Massar <weloxux@glowbug.nl>
+-- Sprites: Brzoz <email and stuff>
+
+local loader = require "libs/AdvTileLoader/Loader" -- Might not be needed
+
+loader.path = maps/
+
+function love.load()
+	-- code
+end
+
+function love.update(dt)
+	-- code
+end
+
+function love.draw()
+	map:draw()
+end
