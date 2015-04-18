24aa51271ba1eeebddf7055a7871d2a9b7166151
Marnix Massar <weloxux@glowbug.nl>
2015-04-18 04:20:32 +0200

    push

--- main.lua
+++ main.lua
@@ -6,8 +6,21 @@
 -- Sprites: Brzoz <email and stuff>
 
 local loader = require "libs/AdvTileLoader/Loader" -- Might not be needed
+local gamestate = require "libs/HUMP/gamestate"
 
-loader.path = maps/
+local menu = {} -- Define gamestates
+local level1 = {}
+
+loader.path = "maps/"
+
+-- Manage resources (inspired by vrld's Princess):
+local function Proxy(f)
+	return setmetatable({}, {__index = function(self, k)
+		local v = f(k)
+		rawset(self, k, v)
+		return v
+	end})
+end
 
 function love.load()
 	-- code
