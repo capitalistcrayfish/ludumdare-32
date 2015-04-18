e52c205115131cab714ffce02a607314dd4363e3
Marnix Massar <weloxux@glowbug.nl>
2015-04-18 08:59:50 +0200

    <descriptive comment here :^)>

--- main.lua
+++ main.lua
@@ -14,7 +14,8 @@ local boss1 = {}
 
 love.graphics.setNewFont(32)
 
-
+manus = {status = neutral, x = 450, y = 600, img = nil}
+speed = 100
 
 -- Manage resources (inspired by vrld's Princess):
 local function Proxy(f)
@@ -29,13 +30,49 @@ Img = Proxy( function(k) return loves.graphics.newImage("img/"..k..".png") end)
 Sound = Proxy(function(k) return love.sound.newSoundData("sound/"..k..".ogg") end)
 Music = Proxy(function(k) return k, "stream" end)
 
+function level1:init()
+	waveTimer = 12
+	waveTimerMax = 12
+
+	coolDown = 0
+end
+
+function level1:enter(previous, ...)
+	manus = {status = neutral, x = 450, y = 600, img = nil}
+	speed = 100
+
+	files = {}
+	scissors = {}
+end
 
 function level1:update(dt)
-	-- code
+
+	if love.keyboard.isDown("left") then
+		if manus.x > 0 then
+			manus.x = manus.x - (speed * dt)
+		end
+	end
+	if love.keyboard.isDown("right") then
+		if manus.x < love.graphics.getWidth() - manus.img:getWidth() then
+			manus.x = manus.x + (speed * dt)
+		end
+	end
+	if love.keyboard.isDown("up") then
+		if manus.y > then
+			manus.y = manus.y - (0.5 * speed * dt)
+		end
+	end
+	if love.keyboard.isDown("down") then
+		if manus.y < love.graphics.getHeight() - manus.img:getHeight() then
+			manus.y = manus.y + (0.5 * speed * dt)
+		end
+	end
+
+
 end
 
 function level1:draw()
-	love.graphics.print(":)", 0, 34)
+	love.graphics.rectangle("fill", manus.x, manus.y, 50, 50)
 end
 
 function level1:quit()
