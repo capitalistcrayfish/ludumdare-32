f06b4d0dac4e263388ac411493107df6b815fa01
Marnix Massar <weloxux@glowbug.nl>
2015-04-20 21:31:35 +0200

    Add icon to conf.lua

--- conf.lua
+++ conf.lua
@@ -2,6 +2,7 @@ function love.conf(t)
 	t.title = "Operation H.A.N.D.s"
 	t.version = "0.9.1"
 	t.identity = "operation-hands"
+	t.icon = "img/hand.ico"
 
 	t.window.width = 900
 	t.window.height = 900
